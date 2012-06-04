13f3e14960b2ea52688a35f59808865730bd707f
Mario <mario.barrenechea@colorado.edu>
2012-06-03 22:49:17 -0600

    (CDP: CODE IN DEVELOPMENT) - Changed much of the overall codebase structure to increase flexibility for incoming development: 1) increased security by re-moving the WSGI script outside of the Django project to avoid having Apache serve it. 2) re-named all nplh references to 'project', which hereby refers to the django project directory inside of the /epm directory, which hereby refers to the workspace directory containing all codebase and configuration files. There is much work to do.

---
+++ epm/auth/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+from auth.views import home, done, logout, error, form
+
+urlpatterns = patterns('',
+    url(r'^$', home, name='home'),
+    url(r'^done/$', done, name='done'),
+    url(r'^error/$', error, name='error'),
+    url(r'^logout/$', logout, name='logout')
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
