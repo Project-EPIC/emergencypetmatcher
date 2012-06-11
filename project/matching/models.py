469beb7820d29a01510679355f53aa706cf589fb
Mario <mario.barrenechea@colorado.edu>
2012-06-10 18:18:46 -0600

    Committing the matching django app, as well as its models and tests.

--- project/matching/models.py
+++ project/matching/models.py
@@ -1,34 +1,36 @@
 from django.db import models
-from djangotoolbox.fields import ListField, EmbeddedModelField
+from djangotoolbox.fields import ListField, EmbeddedModelField, DictField
 from django_mongodb_engine.contrib import MongoDBManager
 from reporting.models import PetReport, Pets
 
 # Create your models here.
 
-class User (models.Model):
-	name = models.CharField(max_length=100, null=False, blank=False)
-
 #The Pet Match Object Model
 class PetMatch(models.Model):
-	lost_pet = EmbeddedModelField(PetReport)
-	found_pet = EmbeddedModelField(PetReport)
-	proposed_by = EmbeddedModelField(User)
-	is_open = models.BooleanField()
+	lost_pet = EmbeddedModelField(PetReport, null=False)
+	found_pet = EmbeddedModelField(PetReport, null=False)
+	proposed_by = EmbeddedModelField(User, null=False)
+	proposed_date = models.DateTimeField(auto_now_add = True)
+	is_open = models.BooleanField(default=True)
 	#It is OK that this field is null (initially)
 	up_votes = ListField(EmbeddedModelField(User), null=True)
 	down_votes = ListField(EmbeddedModelField(User), null=True)
-	score = models.IntegerField(null=True)
+	score = models.IntegerField(default = 0)
 	closed_by = EmbeddedModelField(User, null=True)
+	closed_date = models.DateTimeField(null=True)
 
 	def __unicode__ (self):
-		return '{objid: %s - %s:%s }' % (self.id, self.lost_pet, self.found_pet)
-
+		return 'PetMatch {%s:%s}' % (self.lost_pet, self.found_pet)
 
 #The Chat Object Model
 class Chat (models.Model):
-	name = models.CharField(max_length=100)
-	pet_report = EmbeddedModelField(PetReport)
-	current_users = ListField(EmbeddedModelField(User))
-	#content has a List of Lists of [User, text, date]
-	content = ListField(ListField(EmbeddedModelField(User), 
-		models.CharField(blank=False), models.DateTimeField()))
+	pet_report = EmbeddedModelField(PetReport, null=False)
+	current_users = ListField(EmbeddedModelField(User), null=True)
+	#content has a List of DictFields of {User, text, date+time}. Note that the auto_now option means that
+	#once a dictfield has been added or edited to this object instance, the date and time is recorded here.
+	content = ListField(DictField(EmbeddedModelField(User, null=False), 
+		models.CharField(max_length=1000, blank=True), models.DateTimeField(auto_now_add=True)))
+
+	def __unicode__ (self):
+		return 'Chat {pet_report:%s}' % (self.pet_report)
+
--- project/matching/tests.py
+++ project/matching/tests.py
@@ -7,14 +7,14 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 from reporting.models import Pets, PetReport
 from matching.models import PetMatch, Chat, User
-import random
+import random, datetime
 
 class MatchingTest(TestCase):
 
 	#Control Variable
 	NUMBER_OF_TESTS = 100
 
-   	def setUp(self):
+	def setUp(self):
 		#Get rid of all objects in the QuerySet.
 		PetMatch.objects.all().delete()
 		Chat.objects.all().delete()
@@ -24,38 +24,80 @@ class MatchingTest(TestCase):
 		PetMatch.objects.all().delete()
 		Chat.objects.all().delete()
 
-'''''''''''''''''''''''''''''''''''''''''''''''''''
-CRUD Tests for: Chat
-'''''''''''''''''''''''''''''''''''''''''''''''''''
+	'''''''''''''''''''''''''''''''''''''''''''''''''''
+	CRUD Tests for: Chat
+	'''''''''''''''''''''''''''''''''''''''''''''''''''
 
 	def test_saveChat(self):
-		for i in range (self.NUMBER_OF_TESTS):
+		print '>>>> Testing test_saveChat for %d iterations' % self.NUMBER_OF_TESTS
 
-			chat = Chat(pet_report = PetReport(lost=True))
+		for i in range(self.NUMBER_OF_TESTS):
+
+			pr = PetReport(lost=True)
+			chat = Chat(pet_report = pr)
 			chat.save()
-			chat_same = Chat.objects.get(name = str(i))
+			chat_same = Chat.objects.get(pet_report=pr)
 			self.assertEqual(chat, chat_same)
+			self.assertEqual(chat.pet_report, chat_same.pet_report)
 
+		self.assertTrue(len(Chat.objects.all()) == self.NUMBER_OF_TESTS)
 
 	def test_updateChat(self):
+		print '>>>> Testing test_updateChat for %d iterations' % self.NUMBER_OF_TESTS
+
+		for i in range (self.NUMBER_OF_TESTS):
+
+			#First, create the Chat object
+			pr = PetReport(lost=True)
+			chat = Chat.objects.create(pet_report = pr)
+
+			#UPDATE: chat content
+			chatContent = chat.content
+			self.assertTrue(len(chatContent) == 0)
+			chatContent.append({'user': User(name="testUser"), 
+				'content': "TEST_CONTENT", 'date': datetime.datetime.now()})
+
+			#Save it to the database.
+			chat.save()
+			chat_updated = Chat.objects.get(name = str(i - 1))
+
+			#Now assert that the updated Chat matches the one we've just updated.
+			self.assertEqual(chat, chat_updated)
+
+		
+	def test_deleteChat(self):
+		print '>>>> Testing test_deleteChat for %d iterations' % self.NUMBER_OF_TESTS
+
+		for i in range (self.NUMBER_OF_TESTS):
+
+			#First, create the Chat object.
+			pm = Chat.objects.create ( name = str(i), pet_report = PetReport(lost=True))
+
+			#Delete that (specific) one.
+			Chat.objects.all().get(name = str(i)).delete()
+
+			#Assert that there is nothing in the database!
+			self.assertTrue(len(Chat.objects.all()) == 0)
+
+		self.assertTrue(len(Chat.objects.all()) == 0)
+
 		pass
 
-	def test_deleteChat(self):
-
-'''''''''''''''''''''''''''''''''''''''''''''''''''
-CRUD Tests for: PetMatch 
-'''''''''''''''''''''''''''''''''''''''''''''''''''
+	'''''''''''''''''''''''''''''''''''''''''''''''''''
+	CRUD Tests for: PetMatch 
+	'''''''''''''''''''''''''''''''''''''''''''''''''''
 
 	#SAVE+READ Operation
 	def test_savePetMatch(self):
+		print '>>>> Testing test_savePetMatch for %d iterations' % self.NUMBER_OF_TESTS
 
 		for i in range (self.NUMBER_OF_TESTS):
 
 			#First, create the PetMatch object.
 			pm = PetMatch(	lost_pet = PetReport(lost = True), 
-							found_pet = PetReport (lost = False), 
-							proposed_by = User(name= "testUser"),
-							score = i )
+				found_pet = PetReport (lost = False), 
+				proposed_by = User(name= "testUser"),
+				score = i )
 
 			#Save it to the database.
 			pm.save()
@@ -69,20 +111,16 @@ CRUD Tests for: PetMatch
 
 
 	#UPDATE Operation
-	def test_readPetMatch(self):
+	def test_updatePetMatch(self):
+		print '>>>> Testing test_updatePetMatch for %d iterations' % self.NUMBER_OF_TESTS
 
 		for i in range (self.NUMBER_OF_TESTS):
 
-			#First, create the PetMatch object.
+			#First, create and save the PetMatch object.
 			pm = PetMatch.objects.create (	lost_pet = PetReport(lost = True), 
-											found_pet = PetReport (lost = False), 
-											proposed_by = User(name= "testUser"),
-											score = i )
-
-			pm_same = PetMatch.objects.get(score = i)
-
-			#Assert that they are the same
-			self.assertEqual(pm, pm_same)
+				found_pet = PetReport (lost = False), 
+				proposed_by = User(name= "testUser"),
+				score = i )
 
 			#UPDATE: score
 			pm.score = i - 1
@@ -93,17 +131,19 @@ CRUD Tests for: PetMatch
 
 			#Now assert that the updated PetMatch matches the one we've just updated.
 			self.assertEqual(pm, pm_updated)
+			self.assertEqual(pm.score, pm_updated.score)
 
 
 	def test_deletePetMatch(self):
+		print '>>>> Testing test_deletePetMatch for %d iterations' % self.NUMBER_OF_TESTS
 
 		for i in range (self.NUMBER_OF_TESTS):
 
 			#First, create the PetMatch object.
 			pm = PetMatch.objects.create (	lost_pet = PetReport(lost = True), 
-											found_pet = PetReport (lost = False), 
-											proposed_by = User(name= "testUser"),
-											score = i )
+				found_pet = PetReport (lost = False), 
+				proposed_by = User(name= "testUser"),
+				score = i )
 
 			PetMatch.objects.all().get(score = i).delete()
 
