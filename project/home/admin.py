2b3f9afb5aad816346501aae517842169192f1a2
Amrutha <amrutha.rajiv@gmail.com>
2012-08-02 18:36:16 -0600

    More changes to admin.py

--- project/home/admin.py
+++ project/home/admin.py
@@ -9,10 +9,10 @@ class PetReportAdmin(admin.ModelAdmin):
         })
     ]
     
-    list_display = ('pet_type', 'status', 'date_lost_or_found','proposed_by','sex','size','location','pet_name','age','color','breed') 
+    list_display = ('id','pet_type', 'status', 'date_lost_or_found','proposed_by','sex','size','location','pet_name','age','color','breed') 
 
 class PetMatchAdmin(admin.ModelAdmin):   
-    list_display = ('lost_pet','found_pet','proposed_by','proposed_date','score','closed_by','closed_date','matches_proposed')
+    list_display = ('id','lost_pet','found_pet','proposed_by','proposed_date','score','closed_by','closed_date')
 
 class UserProfileInline(admin.StackedInline):
 	model = UserProfile
@@ -21,9 +21,13 @@ class UserAdmin(admin.ModelAdmin):
 	inlines = [
 		UserProfileInline,
 	]
+	list_display = ('id','username','first_name','last_name','email','is_staff','is_superuser','is_active')
+
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('id','user','reputation')	
 
 admin.site.register(PetReport,PetReportAdmin)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile,UserProfileAdmin)
 admin.site.register(PetMatch,PetMatchAdmin)
 admin.site.unregister(User)
 admin.site.register(User,UserAdmin)
