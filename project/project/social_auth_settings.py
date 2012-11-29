c902425d904321c814a3c9e4ab7e01c3af27b414
Sahar Jambi <sahar.jambi@colorado.edu>
2012-11-29 01:16:52 -0700

    Comitting the inital code of Tastypie

--- project/project/social_auth_settings.py
+++ project/project/social_auth_settings.py
@@ -34,7 +34,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.social.associate_user',
     'social_auth.backends.pipeline.social.load_extra_data',
     'social_auth.backends.pipeline.user.update_user_details',
-    'social_auth.backends.pipeline.misc.save_status_to_session',
     'home.pipeline.get_user_avatar',
     'home.pipeline.create_user_log',
 
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,5 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from tastypie.api import Api
+from home.api import UserResource, UserProfileResource, PetReportResource
+
+user_resource = UserResource()
+userprofile_resource = UserProfileResource()
+petreport_resource = PetReportResource()
+
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(UserProfileResource())
+v1_api.register(PetReportResource())
+
 
 admin.autodiscover()
 
@@ -10,5 +22,10 @@ urlpatterns = patterns('',
 	url (r'^matching/', include ('matching.urls')),
 	# Uncomment the next line to enable the admin:
     url (r'^admin/', include(admin.site.urls)),
-    
+
+	url (r'^api/', include(user_resource.urls)),
+	url (r'^api/', include(userprofile_resource.urls)),
+    url (r'^api/', include(petreport_resource.urls)),    
+    url (r'^api/', include(v1_api.urls)),
+
 )
