c1d4d0fe2a2ea1087ed2c52cd4b57243ac2b2813
amrutha <amrutha.rajiv@gmail.com>
2012-12-09 18:27:11 -0700

    Removed the additional resource PetReportResource2 and made changes to PetReportResource so that the petreport allows the posting of an image

--- project/home/api.py
+++ project/home/api.py
@@ -28,72 +28,6 @@ class UserProfileResource(ModelResource):
         queryset = UserProfile.objects.all()
         resource_name = 'userprofile'
 
-class PetReportResource(ModelResource):
-    proposed_by = fields.ForeignKey(UserProfileResource, 'proposed_by', full=True)
-    
-    class Meta:
-        queryset = PetReport.objects.all()
-        resource_name = 'petreport'
-        allowed_methods = ['get', 'post']
-        authentication = BasicAuthentication()
-        authorization = Authorization()
-        always_return_data=True
-
-    def obj_create(self, bundle, request=None, **kwargs):
-        
-        try: 
-            # Read sent input data from bundle object
-            '''Required Fields'''
-            pet_type = bundle.data['pet_type']
-            status = bundle.data['status']
-            date_lost_or_found = datetime.strptime(bundle.data['date_lost_or_found'], '%m/%d/%Y')
-            sex = bundle.data['sex']
-            size = bundle.data['size']
-            location = bundle.data['location']
-            proposed_by = request.user.get_profile()
-            '''Non-Required Fields'''
-            img_path = ""
-            pet_name = ""
-            age= ""
-            color = ""
-            breed = ""
-            description = ""
-            if 'img_path' in bundle.data: img_path = bundle.data['img_path']
-            if img_path == "":
-                if pet_type == "Dog":
-                    img_path.name = "images/defaults/dog_silhouette.jpg"
-                elif pet_type == "Cat":
-                    img_path = "images/defaults/cat_silhouette.jpg"
-                elif pet_type == "Horse":
-                    img_path.name = "images/defaults/horse_silhouette.jpg"
-                elif pet_type == "Rabbit":
-                    img_path.name = "images/defaults/rabbit_silhouette.jpg"
-                elif pet_type == "Snake":
-                    img_path.name = "images/defaults/snake_silhouette.jpg"                                       
-                elif pet_type == "Turtle":
-                    img_path.name = "images/defaults/turtle_silhouette.jpg"
-                else:
-                    img_path.name = "images/defaults/other_silhouette.jpg"
-            # if img_path is not found
-            if 'pet_name' in bundle.data: pet_name = bundle.data['pet_name']
-            if 'age' in bundle.data: age = bundle.data['age']
-            if 'color' in bundle.data: color = bundle.data['color']
-            if 'breed' in bundle.data: breed = bundle.data['breed']
-            if 'description' in bundle.data: description = bundle.data['description']
-            # Create a PetReport object
-            try:
-                pr = PetReport(pet_type=pet_type, status=status, date_lost_or_found=date_lost_or_found, 
-                    sex=sex, size=size, location=location, proposed_by=proposed_by, 
-                    img_path=img_path, pet_name=pet_name, age=age, color=color, breed=breed, description=description)
-                bundle.obj = pr
-                bundle.obj.save()
-                log_activity(ACTIVITY_PETREPORT_SUBMITTED, proposed_by, petreport=pr)
-            except IntegrityError:
-                raise BadRequest('That petreport already exists\n')
-        except Exception:
-            raise BadRequest('Please include all the required fields to submit a petreport\n')
-        return bundle
-
 class MultipartResource(object):
     def deserialize(self, request, data, format=None):
         if not format:
@@ -106,12 +40,12 @@ class MultipartResource(object):
             return data
         return super(MultipartResource, self).deserialize(request, data, format)
 
-class PetReportResource2(MultipartResource, ModelResource):
+class PetReportResource(MultipartResource, ModelResource):
     proposed_by = fields.ForeignKey(UserProfileResource, 'proposed_by', full=True)
     
     class Meta:
         queryset = PetReport.objects.all()
-        resource_name = 'petreport2'
+        resource_name = 'petreport'
         # include_resource_uri = False
         allowed_methods = ['get', 'post']
         authentication = BasicAuthentication()
@@ -172,4 +106,3 @@ class PetReportResource2(MultipartResource, ModelResource):
         except Exception:
             raise BadRequest('Please include all the required fields to submit a petreport\n')
         return bundle
-    
