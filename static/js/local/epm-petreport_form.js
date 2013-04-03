107d00a750f367a8c26a8df9378b5aa81d89fd66
amrutha <amrutha.rajiv@gmail.com>
2013-04-03 03:29:59 -0600

    changed Tag Information and Pet Description lengths to 1000 characters, Edited description at the top of submit pet report page, added javascript to perform checks after submit is clicked, added j/s to alert the user if the image size is > 3MB, added j/s to show count of words in the text area boxes for tag information and pet description, added a check to make sure the date field has only current and past dates but no future dates

--- static/js/local/epm-petreport_form.js
+++ static/js/local/epm-petreport_form.js
@@ -1,4 +1,17 @@
 
+	var	PETREPORT_TAG_INFO_LENGTH = 1000;
+	var PETREPORT_DESCRIPTION_LENGTH = 1000;
+	function countChars(textbox, counter, max) {
+		var count = max - document.getElementById(textbox).value.length;
+		if (count < 0) { 
+			document.getElementById(counter).innerHTML = "<span style=\"color: red;\">" + count +" characters remaining" + "</span>"; 
+		}
+	  	else { 
+	  		document.getElementById(counter).innerHTML = count+" characters remaining"; 
+	  	}
+
+	}
+
 $(document).ready(function(){
 
 
@@ -26,5 +39,62 @@ $(document).ready(function(){
 			$("#petreport_form_contact_fields").css("display", "none");
 	});		
 
+	var img_size = 0.0;
 
+	$('#id_img_path').bind('change', function() {
+           	img_size = this.files[0].size/1024/1024;
+            if (img_size > 3.0) 
+            	alert("Image size exceeds 3MB, please upload an image that is within 3MB");
+    });
+
+
+	$("#id_submit").click(function(){
+
+			var conditions = true;
+			var issues = "Please fix the following fields before submitting this form: \n";
+			var num_issues = 0;
+			if (img_size > 3.0)
+			{
+				conditions = false;
+				
+				issues += (++num_issues)+". Image size should be less that 3MB\n";
+
+			}		
+			var description = $("#id_description").attr("value").length;
+			if ( description> PETREPORT_DESCRIPTION_LENGTH){
+
+				conditions = false;
+				var exceeds = description - PETREPORT_DESCRIPTION_LENGTH;
+				issues += (++num_issues)+". Pet description should be within "+PETREPORT_DESCRIPTION_LENGTH+" characters, your description is "+exceeds+" characters over that limit.\n"
+			}
+			var tag_info_length = $("#id_tag_info").attr("value").length;
+			if ( tag_info_length> PETREPORT_TAG_INFO_LENGTH){
+
+				conditions = false;
+				var exceeds = tag_info_length - PETREPORT_TAG_INFO_LENGTH ;
+				alert (exceeds);
+				issues += (++num_issues)+". Tag and Collar information should be within "+PETREPORT_TAG_INFO_LENGTH+" characters, the content you entered is "+exceeds+" characters over that limit.\n"
+			}
+
+			/*check for date value = null and date value in future*/
+			if ($("#id_date_lost_or_found").attr("value")==""){
+				conditions = false;
+				issues += (++num_issues)+" . Please fill in the Date field";
+			}
+
+			else if ((new Date($("#id_date_lost_or_found").attr("value"))) > (new Date()) ){
+				conditions = false;
+				issues += (++num_issues)+" . Please fill in a valid Date";
+			}
+
+			if(conditions){
+				document.forms['petreport_form'].submit();
+			}
+			else{
+				alert(issues);
+				
+			}
+
+	});
 });
+
