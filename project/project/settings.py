4c061cbf77c30f89baaf2ebcc247ae921e5e65e2
Mario Barrenechea <mario.barrenechea@colorado.edu>
2013-08-20 16:38:16 -0600

    added server database settings but doc'ed out.

--- project/project/settings.py
+++ project/project/settings.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.  
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'epm_db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
