edc0df47f63d3f23e79ba9180931041571beef25
Mario Barrenechea <mario.barrenechea@colorado.edu>
2013-08-21 11:34:53 -0600

    Adding control servers script that will aid staging and production and (eventually) the dev environment.

---
+++ project/controlservers.py
@@ -0,0 +1,18 @@
+import os, sys, subprocess
+
+if ((len(sys.argv) != 2) or ((sys.argv[1] != "stop") and (sys.argv[1] != "start"))):
+	print "Usage: python controlservers.py <arg>"
+	print "\t arg = {stop, start}"
+	sys.exit(1)
+
+command = sys.argv[1]
+print "%sing Nginx and Apache Servers..." % command
+
+if command == "start":
+	subprocess.call("sudo nginx -s start")
+	subprocess.call("sudo service apache2 start")
+
+elif command == "stop":
+	subprocess.call("sudo nginx -s stop")
+	subprocess.call("sudo service apache2 stop")
+
