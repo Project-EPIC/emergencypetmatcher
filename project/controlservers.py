311589b529c353ca6532ed47adacf34f4f221cc5
Mario Barrenechea <mario.barrenechea@colorado.edu>
2013-08-21 12:14:08 -0600

    Added 'restart' to control servers and refactored.

--- project/controlservers.py
+++ project/controlservers.py
@@ -1,18 +1,13 @@
 import os, sys, subprocess
 
-if ((len(sys.argv) != 2) or ((sys.argv[1] != "stop") and (sys.argv[1] != "start"))):
+if len(sys.argv) != 2 or sys.argv[1] not in ["stop", "start", "restart"]:
 	print "Usage: python controlservers.py <arg>"
-	print "\t arg = {stop, start}"
+	print "\t arg = {stop, start, restart}"
 	sys.exit(1)
 
 command = sys.argv[1]
 print "%sing Nginx and Apache Servers..." % command
 
-if command == "start":
-	subprocess.call(["sudo", "service", "nginx", "start"])
-	subprocess.call(["sudo", "service", "apache2", "start"])
-
-elif command == "stop":
-	subprocess.call(["sudo", "service", "nginx", "stop"])
-	subprocess.call(["sudo", "service", "apache2", "stop"])
-
+#Execute process calls.
+subprocess.call(["sudo", "service", "nginx", command])
+subprocess.call(["sudo", "service", "apache2", command])
