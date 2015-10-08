26f62932d73af528c7cf74b1c099f48130f62c57
Mario Barrenechea <mbarrenecheajr@gmail.com>
2015-10-07 18:42:28 -0600

    enhanced stats flask page.

--- project/stats.py
+++ project/stats.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template
 from fixture import *
+from django.db.models import Count
 import pdb
 
 app = Flask(__name__)
 
 @app.route("/")
-def status():
-  return "UP"
-
-@app.route("/stats/")
 def stats():
   results = {}
   results["num_users"] = User.objects.count()
@@ -16,6 +13,8 @@ def stats():
   results["num_lost_petreports"] = PetReport.objects.filter(status="Lost").count()
   results["num_found_petreports"] = PetReport.objects.filter(status="Found").count()
   results["num_petmatches"] = PetMatch.objects.count()
+  top_5_reporters = UserProfile.objects.annotate(num_reports=Count("proposed_related")).order_by('-num_reports')[:5]
+  results["top_5_reporters"] = top_5_reporters
   return render_template("stats.html", results=results)
 
 if __name__ == "__main__":
